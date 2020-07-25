07d6e39cd2763196da9ec22469d5e7a182e7335a
Marcus Lee Eugene <mctjl_1997@yahoo.com.sg>
2020-07-26 01:13:35 +0800

    Completed handling of OnPress of trail text

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 
 export default function BreadCrumb({ trailData, selectedIndex, onPress, backgroundColor, fontColor, fontSize, height, top, textSpacing}) {
 
   const handleOnPress = (index) => {
-    return index;
+    if (selectedIndex !== index) {
+      onPress(index);
+    }
   }
 
   const breadCrumb = (
-    <View style={styles.breadCrumbContainer}>
-      {
-        trailData.map((item, index) => (
-          (item == trailData[selectedIndex])
-            ? <Text key={index} style={[styles.text, styles.selected, {color: fontColor, fontSize: fontSize, paddingHorizontal: textSpacing}]} onPress={(index) => handleOnPress(index)}> {item} </Text>
-            : <Text key={index} style={[styles.text, {color: fontColor, fontSize: fontSize, paddingHorizontal: textSpacing}]} onPress={(index) => handleOnPress(index)}> {item} </Text>
-        ))
-      }
-    </View>
+    trailData.map((item, index) => (
+      (item == trailData[selectedIndex])
+        ? <Text
+            key={index}
+            style={[styles.text, styles.selected, {color: fontColor, fontSize: fontSize, paddingHorizontal: textSpacing}]}
+            onPress={() => handleOnPress(index)}>
+            {item}
+          </Text>
+        : <Text
+            key={index}
+            style={[styles.text, {color: fontColor, fontSize: fontSize, paddingHorizontal: textSpacing}]}
+            onPress={() => handleOnPress(index)}>
+            {item}
+          </Text>
+    ))
   );
 
   return (
-    <View style={[styles.container, {height: height}, {backgroundColor: backgroundColor}, {top: top}]}>
+    <View
+      style={[styles.container, {height: height}, {backgroundColor: backgroundColor}, {top: top}]}>
       {breadCrumb}
     </View>
   );
@@ -47,10 +56,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
-  },
-  breadCrumbContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
     flexDirection: 'row'
   },
   selected: {
